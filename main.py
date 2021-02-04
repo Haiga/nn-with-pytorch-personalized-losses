05609ddb0f0939d8dea2e9e350194b1fbfe97b87
Pedro <pedro_rga@hotmail.com>
2021-02-04 16:08:32 -0300

    add lambdaLoss, approxNDCG (from Context-Aware Learning to Rank with Self-Attention) + utils datasets

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
+
 # torch.seed = 1000
+from losses.approxNDCG import approxNDCGLoss
+from utils.dataset import get_data, svmDataset
+from losses.lambdaLoss import lambdaLoss
+
 
 class Net(nn.Module):
     def __init__(self, N_features):
@@ -24,38 +29,101 @@ def listnet_loss(y_true, y_predicted):
     z_i: (n_i, 1)
     """
 
-    P_y_true = F.softmax(y_true, dim=1)
-    P_y_predicted = F.softmax(y_predicted, dim=1)
+    P_y_true = torch.squeeze(F.softmax(y_true, dim=1))
+    P_y_predicted = torch.squeeze(F.softmax(y_predicted, dim=1))
     return - torch.sum(P_y_true * torch.log(P_y_predicted))
 
-def ndcg_loss(y_true, y_predicted):
-    """
-    y_i: (n_i, 1)
-    z_i: (n_i, 1)
-    """
+def clean_ndcg_loss(y_true, y_predicted):
 
     y_true = F.softmax(y_true, dim=1)
     y_predicted = F.softmax(y_predicted, dim=1)
+    y_predicted= torch.log(y_predicted)
 
-    corrected_order = torch.argsort(y_true, descending=True, dim=1)
+    corrected_order_y_true = torch.argsort(y_true, descending=True, dim=1)
     ordered_scores_y_predicted = []
-    ordered_scores_y_true = []
-    # TODO evitar loops e stack
-    for i in range(y_predicted.shape[0]):
-        ordered_scores_y_predicted.append(torch.squeeze(y_predicted[i][corrected_order[i]]))
-        ordered_scores_y_true.append(torch.squeeze(y_true[i][corrected_order[i]]))
-    ordered_scores_y_predicted = torch.stack(ordered_scores_y_predicted)
-    ordered_scores_y_true = torch.stack(ordered_scores_y_true)
-    inverted_gains = torch.log(torch.arange(1, y_predicted.shape[1] + 1, dtype=torch.float, requires_grad=True)) + 1
-    dcg = torch.sum(ordered_scores_y_predicted/inverted_gains, dim=1)
-    idcg = torch.sum(ordered_scores_y_true/inverted_gains, dim=1)
+    # ordered_scores_y_true = []
 
-    return torch.mean(-(dcg/idcg))
-    # return -(dcg/idcg)
+    for i in range(y_predicted.shape[0]):
+        ordered_scores_y_predicted.append(torch.squeeze(y_predicted[i][corrected_order_y_true[i]]))
+        # ordered_scores_y_true.append(torch.squeeze(y_true[i][corrected_order_y_true[i]]))
+
+    ordered_scores_y_predicted = torch.stack(ordered_scores_y_predicted)
+    # ordered_scores_y_true = torch.stack(ordered_scores_y_true)
+
+    # inverted_gains = torch.log(torch.arange(1, y_predicted.shape[1] + 1, dtype=torch.float, requires_grad=True)) + 1
+    # decrescing_gains = torch.log(torch.arange(-y_predicted.shape[1], y_predicted.shape[1], 2, dtype=torch.float, requires_grad=True))
+
+    decrescing_gains = torch.arange(-y_predicted.shape[1], y_predicted.shape[1], 2, dtype=torch.float,
+                  requires_grad=True)
+    # decrescing_gains= decrescing_gains ** 3
+    dgc = decrescing_gains * ordered_scores_y_predicted
+
+    # dgc = F.softmax(dgc, dim=1)
+    # y_predicted = F.softmax(y_predicted, dim=1)
+
+    # return torch.mean(torch.sum(dgc, dim=1))
+    return -torch.mean(torch.sum(dgc, dim=1))
+    # return torch.var(torch.sum(dgc, dim=1))
+
+# def ndcg_loss(y_true, y_predicted):
+#
+#     y_true = F.softmax(y_true, dim=1)
+#     y_predicted = F.softmax(y_predicted, dim=1)
+#
+#     corrected_order_y_pred = torch.argsort(y_predicted, descending=True, dim=1)
+#     ordered_scores_y_predicted = []
+#
+#     for i in range(y_predicted.shape[0]):
+#         ordered_scores_y_predicted.append(torch.squeeze(y_true[i][corrected_order_y_pred[i]]))
+#
+#     ordered_scores_y_predicted = torch.stack(ordered_scores_y_predicted)
+#
+#     inverted_gains = torch.log(torch.arange(1, y_predicted.shape[1] + 1, dtype=torch.float, requires_grad=True)) + 1
+#     dcg = torch.sum(ordered_scores_y_predicted / inverted_gains, dim=1)
+#
+#     corrected_order = torch.argsort(y_true, descending=True, dim=1)
+#     ordered_scores_y_true = []
+#     # TODO evitar loops e stack
+#     for i in range(y_predicted.shape[0]):
+#         # ordered_scores_y_predicted.append(torch.squeeze(y_predicted[i][corrected_order_y_pred[i]]))
+#         ordered_scores_y_true.append(torch.squeeze(y_true[i][corrected_order[i]]))
+#
+#     ordered_scores_y_true = torch.stack(ordered_scores_y_true)
+#
+#     idcg = torch.sum(ordered_scores_y_true / inverted_gains, dim=1)
+#
+#     # return torch.mean(-(dcg / idcg))
+#     return torch.mean(-(dcg / idcg))
+#     # return -(dcg/idcg)
+# def ndcg_loss(y_true, y_predicted):
+#     """
+#     y_i: (n_i, 1)
+#     z_i: (n_i, 1)
+#     """
+#
+#     y_true = F.softmax(y_true, dim=1)
+#     y_predicted = F.softmax(y_predicted, dim=1)
+#
+#     corrected_order = torch.argsort(y_true, descending=True, dim=1)
+#     ordered_scores_y_predicted = []
+#     ordered_scores_y_true = []
+#     # TODO evitar loops e stack
+#     for i in range(y_predicted.shape[0]):
+#         ordered_scores_y_predicted.append(torch.squeeze(y_predicted[i][corrected_order[i]]))
+#         ordered_scores_y_true.append(torch.squeeze(y_true[i][corrected_order[i]]))
+#     ordered_scores_y_predicted = torch.stack(ordered_scores_y_predicted)
+#     ordered_scores_y_true = torch.stack(ordered_scores_y_true)
+#     inverted_gains = torch.log(torch.arange(1, y_predicted.shape[1] + 1, dtype=torch.float, requires_grad=True)) + 1
+#     dcg = torch.sum(ordered_scores_y_predicted / inverted_gains, dim=1)
+#     idcg = torch.sum(ordered_scores_y_true / inverted_gains, dim=1)
+#
+#     # return torch.mean(-(dcg / idcg))
+#     return torch.mean((dcg / idcg))
+#     # return -(dcg/idcg)
 
 
 def make_dataset(N_queries_train, N_queries_valid, N_docs_per_query, N_features):
-
+    X, y = get_data("2003_td_dataset", "test")
 
     X_train = torch.randn(N_queries_train, N_docs_per_query, N_features, requires_grad=True)
     X_valid = torch.randn(N_queries_valid, N_docs_per_query, N_features, requires_grad=True)
@@ -66,9 +134,8 @@ def make_dataset(N_queries_train, N_queries_valid, N_docs_per_query, N_features)
 
     ws = torch.randn(N_features, 1)
 
-    #TODO evitar loops e stack
+    # TODO evitar loops e stack
     for i in range(N_queries_train):
-
 
         ys_train_score = torch.mm(X_train[i], ws)
         if i < N_queries_valid:
@@ -86,7 +153,6 @@ def make_dataset(N_queries_train, N_queries_valid, N_docs_per_query, N_features)
         ys_final_train.append(ys_train_rel)
         if i < N_queries_valid:
             ys_final_valid.append(ys_valid_rel)
-
 
     ys_final_train = torch.stack(ys_final_train)
     ys_final_valid = torch.stack(ys_final_valid)
@@ -110,7 +176,8 @@ def swapped_pairs(ys_pred, ys_target):
         break
     return swapped
 
-#TODO vetorizar
+
+# TODO vetorizar
 def ndcg(ys_true, ys_pred):
     def dcg(ys_true, ys_pred):
         _, argsort = torch.sort(ys_pred, descending=True, dim=0)
@@ -119,42 +186,56 @@ def ndcg(ys_true, ys_pred):
         for i, l in enumerate(ys_true_sorted, 1):
             ret += (2 ** l - 1) / np.log2(1 + i)
         return ret
+
     r = []
     for q in range(ys_true.shape[0]):
         ideal_dcg = dcg(ys_true[q], ys_true[q])
         pred_dcg = dcg(ys_true[q], ys_pred[q])
-        r.append(pred_dcg/ ideal_dcg)
+        r.append(pred_dcg / ideal_dcg)
     return np.mean(r)
 
 
 if __name__ == '__main__':
-    # N_queries = 100
-    N_queries_train = 500
-    N_queries_valid = 100
-    N_docs_per_query = 100
-    N_features = 50
-    epochs = 10
-    batch_size_docs = 20
-    batch_size_queries = 20
 
-    X_train, X_valid, ys_train, ys_valid = make_dataset(N_queries_train, N_queries_valid, N_docs_per_query, N_features)
+    dataset_name = "2003_td_dataset"
+    data_infos = svmDataset(dataset_name)
+
+    X_train, y_train = get_data(dataset_name, "test")
+    X_vali, y_vali = get_data(dataset_name, "vali")
+
+    X_train = torch.tensor(X_train, requires_grad=True)
+    y_train = torch.tensor(y_train, requires_grad=True)
+    X_vali = torch.tensor(X_vali, requires_grad=True)
+    y_vali = torch.tensor(y_vali, requires_grad=True)
+
+    # N_queries = 100
+    N_queries_train = 10
+    N_queries_valid = 10
+    N_docs_per_query = 1000
+    N_features = data_infos.num_features
+    epochs = 30
+    # batch_size_docs = 20
+    batch_size_queries = 3
+
+    # X_train, X_valid, ys_train, ys_valid = make_dataset(N_queries_train, N_queries_valid, N_docs_per_query, N_features)
 
     net = Net(N_features)
     # opt = optim.Adam(net.parameters(), lr=0.001)
     opt = optim.Adam(net.parameters())
-    # opt = optim.SGD(net.parameters(), lr=0.1)
+    # opt = optim.SGD(net.parameters(), lr=0.001)
+    # opt = optim.SGD(net.parameters(), lr=0.0001)
 
     for epoch in range(epochs):
         idx = torch.randperm(N_queries_train)
 
         X_train = X_train[idx]
-        ys_train = ys_train[idx]
+        y_train = y_train[idx]
 
         cur_batch = 0
         for it in range(N_queries_train // batch_size_queries):
             # print(f"started {it}")
             batch_X = X_train[cur_batch: cur_batch + batch_size_queries]
-            batch_ys = ys_train[cur_batch: cur_batch + batch_size_queries]
+            batch_ys = y_train[cur_batch: cur_batch + batch_size_queries]
             cur_batch += batch_size_queries
 
             opt.zero_grad()
@@ -166,8 +247,16 @@ if __name__ == '__main__':
                 # batch_preds = torch.stack(batch_preds)
                 batch_preds = net(batch_X)
 
+
+                ####
+                batch_ys = torch.squeeze(batch_ys)
+                batch_preds = torch.squeeze(batch_preds)
+                ##
+                # batch_loss = lambdaLoss(batch_preds, batch_ys)
+                # batch_loss = ndcg_loss(batch_ys, batch_preds)
+                # batch_loss = approxNDCGLoss(batch_ys, batch_preds)
+                batch_loss = clean_ndcg_loss(batch_ys, batch_preds)
                 # batch_loss = listnet_loss(batch_ys, batch_preds)
-                batch_loss = ndcg_loss(batch_ys, batch_preds)
                 batch_loss.backward(retain_graph=True)
                 opt.step()
                 # batch_loss = ndcg_loss(batch_ys, batch_pred)
@@ -177,7 +266,9 @@ if __name__ == '__main__':
                 # #####
             # print(f"ended {it}")
         with torch.no_grad():
-            valid_pred = net(X_valid)
-            valid_swapped_pairs = swapped_pairs(valid_pred, ys_valid)##############TODO demora muito a fazer essa contagem
-            ndcg_score = ndcg(ys_valid, valid_pred).item()
-            print(f"epoch: {epoch + 1} valid swapped pairs: {valid_swapped_pairs}/{1 * N_docs_per_query * (N_docs_per_query - 1) // 2} ndcg: {ndcg_score:.4f}")
+            valid_pred = net(X_vali)
+            # valid_swapped_pairs = swapped_pairs(valid_pred, y_vali)##############TODO demora muito a fazer essa contagem
+            ndcg_score = ndcg(y_vali, valid_pred).item()
+            # ndcg_score = ndcg(y_vali, -1*valid_pred).item()
+            # print(f"epoch: {epoch + 1} valid swapped pairs: {valid_swapped_pairs}/{1 * N_docs_per_query * (N_docs_per_query - 1) // 2} ndcg: {ndcg_score:.4f}")
+            print(f"epoch: {epoch + 1} - ndcg: {ndcg_score:.4f}")
---
+++ utils/dataset.py
@@ -0,0 +1,145 @@
+import numpy as np
+import os
+from sklearn.datasets import load_svmlight_file
+
+
+class svmDataset():
+    def __init__(self, dataset_name):
+        if dataset_name == "2003_td_dataset":
+            self.train_data_path = "D:\\Colecoes\\2003_td_dataset\\Fold1\\train.txt"
+            self.test_data_path = "D:\\Colecoes\\2003_td_dataset\\Fold1\\test.txt"
+            self.vali_data_path = "D:\\Colecoes\\2003_td_dataset\\Fold1\\vali.txt"
+
+            self.baseline_train_data_path = "D:\\Colecoes\\2003_td_dataset\\Fold1\\baseline.train.txt"
+            self.baseline_test_data_path = "D:\\Colecoes\\2003_td_dataset\\Fold1\\baseline.test.txt"
+            self.baseline_vali_data_path = "D:\\Colecoes\\2003_td_dataset\\Fold1\\baseline.vali.txt"
+
+            self.docs_per_query = 1000
+            self.queries_on_test = 10
+            self.queries_on_train = 10
+            self.num_features = 64
+
+            self.normalized_num_docs = True  # TODO false (ver o que fazer)
+
+
+# TODO todas as entradas devem ser formatadas , remover #DOC-id do fim de cada linha do arquivo lib_svm
+
+
+def get_data(dataset_name, type_file="train"):
+    info_dataset = svmDataset(dataset_name)
+
+    if type_file == "train":
+        data = load_svmlight_file(info_dataset.train_data_path, query_id=True)
+    elif type_file == "test":
+        data = load_svmlight_file(info_dataset.test_data_path, query_id=True)
+    elif type_file == "vali":
+        data = load_svmlight_file(info_dataset.vali_data_path, query_id=True)
+
+    labels_by_query = []
+    features_docs_by_query = []
+
+    queries_ids = np.array(data[2])
+    ant = queries_ids[0]
+
+    temp_labels_by_query = []
+    temp_features_docs_by_query = []
+
+    # np.array(data[0][0].shape[1], data[0][0].shape[1])
+    for i in range(queries_ids.size):
+        if ant != queries_ids[i]:
+            labels_by_query.append(temp_labels_by_query)
+            features_docs_by_query.append(temp_features_docs_by_query)
+
+            temp_labels_by_query = []
+            temp_features_docs_by_query = []
+
+        temp_labels_by_query.append(data[1][i])
+        temp_features_docs_by_query.append(data[0][i].toarray().reshape(-1).astype(np.float32))
+        ant = queries_ids[i]
+
+    labels_by_query.append(temp_labels_by_query)
+    features_docs_by_query.append(temp_features_docs_by_query)
+
+    return features_docs_by_query, labels_by_query
+
+
+def load_baseline_file(name_file):
+    with open(name_file, "r") as f:
+        data = f.readlines()
+
+    data_formatted = []
+    queries_ids = []
+    for line in data:
+        splitted_line = line.replace("\n", "").split(",")
+        queries_ids.append(int(splitted_line[0]))
+        data_formatted.append([float(x) for x in splitted_line[1:]])
+
+    data_formatted = np.array(data_formatted, dtype=np.float32)
+    queries_ids = np.array(queries_ids)
+    ant = queries_ids[0]
+
+    baselines_by_query = []
+    temp_baselines_by_query = []
+
+    # np.array(data[0][0].shape[1], data[0][0].shape[1])
+    for i in range(queries_ids.size):
+        if ant != queries_ids[i]:
+            baselines_by_query.append(temp_baselines_by_query)
+            temp_baselines_by_query = []
+
+        temp_baselines_by_query.append(data_formatted[i])
+        ant = queries_ids[i]
+
+    baselines_by_query.append(temp_baselines_by_query)
+
+    return baselines_by_query
+
+
+def store_baseline_data(name_method, dataset_name, queries_ids, predicted_values, append=True, type_file="train"):
+    assert len(queries_ids) == len(predicted_values)
+
+    info_dataset = svmDataset(dataset_name)
+
+    if type_file == "train":
+        name_file = info_dataset.baseline_train_data_path
+    elif type_file == "test":
+        name_file = info_dataset.baseline_test_data_path
+    elif type_file == "vali":
+        name_file = info_dataset.baseline_vali_data_path
+
+    if append:
+        # checar se já existe
+        if not os.path.isfile(name_file):
+            append = False
+    if append:
+        with open(name_file, "r") as existing_file:
+            data = existing_file.readlines()
+        with open(name_file.replace("baseline", "baseline.info"), "a") as existing_file:
+            existing_file.write(name_method + "\n")
+
+    with open(name_file, "w") as f:
+        if append:
+            assert len(queries_ids) == len(data)
+            for i in range(len(queries_ids)):
+                # f.write(f"{queries_ids[i]},{predicted_values[i]}\n")
+                current_line = data[i].replace("\n", "")
+                f.write(f"{current_line},{round(predicted_values[i], ndigits=5)}\n")
+
+        else:
+            for i in range(len(queries_ids)):
+                f.write(f"{queries_ids[i]},{round(predicted_values[i], ndigits=5)}\n")
+            with open(name_file.replace("baseline", "baseline.info"), "w") as existing_file:
+                existing_file.write(name_method + "\n")
+
+
+def get_baseline_data(dataset_name, type_file="train"):
+    info_dataset = svmDataset(dataset_name)
+
+    if type_file == "train":
+        data = load_baseline_file(info_dataset.baseline_train_data_path)
+    elif type_file == "test":
+        data = load_baseline_file(info_dataset.baseline_test_data_path)
+    elif type_file == "vali":
+        data = load_baseline_file(info_dataset.baseline_vali_data_path)
+
+    return data

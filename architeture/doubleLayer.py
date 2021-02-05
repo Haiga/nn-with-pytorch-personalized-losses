b8c5127c813dc2f5c274c2ab4bcdc5c5d49b069a
Pedro <pedro_rga@hotmail.com>
2021-02-05 20:35:48 -0300

    adjusting doubleLayer.py

--- architeture/doubleLayer.py
+++ architeture/doubleLayer.py
@@ -5,8 +5,8 @@ from torch import nn
 class DoubleLayerNet(nn.Module):
     def __init__(self, N_features):
         super(DoubleLayerNet, self).__init__()
-        self.l1 = nn.Linear(N_features, 10)
-        self.l2 = nn.Linear(10, 1)
+        self.l1 = nn.Linear(N_features, N_features)
+        self.l2 = nn.Linear(N_features, 1)
 
     def forward(self, x, mask, indices):
         x = torch.sigmoid(self.l1(x))
